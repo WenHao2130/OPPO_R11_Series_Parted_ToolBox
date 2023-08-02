fd634371ed2b6e313cd5d72797d3c3871992a670
WenHao2130 <98936399+WenHao2130@users.noreply.github.com>
2023-08-02 19:54:01 +0800

    Add Function
    - Add the function of making dual system

--- OPPO_R11_Series_Parted_ToolBox.c
+++ OPPO_R11_Series_Parted_ToolBox.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define VERSION "2.3"
+#define VERSION "2.4"
 void system_plus(const char *command); //基于system函数,但是加了命令是否执行成功判断
 void bar1(void); //分隔栏1(=====)
 void bar2(void); //分隔栏2(-----)
 void confirm_operation(void); //询问用户是否继续操作
 void safe_flush(FILE *fp); //清除stdin函数(抄的)
 void init(void); //检测设备连接状态并推送parted
-void umount_partition(void); //解除挂载System,Vendor,Data分区
-void parted_rm_partition1(void); //删除System,Data分区
-void parted_rm_partition2(void); //删除System,Vendor,Data分区
+void umount_partition(void); //解除挂载System,Vendor,Userdata分区
+void parted_rm_partition1(void); //删除System,Userdata分区
+void parted_rm_partition2(void); //删除System,Vendor,Userdata分区
+void parted_rm_partition3(void); //删除System,System_bak,Vendor,Userdata,Userdata_bak分区
 void clearscreen(void); //替换函数内CLS为clear即可兼容Linux/UNIX
 void checkfile(char *filename); //检查文件是否存在
 void check_platfrom_and_parted_windows(void); //适用于Windows的启动程序检查Platfrom-Tool和Parted是否存在
@@ -21,9 +22,14 @@ int main(void)
 {
 	const int system_partition_start = 5604;
 	int system_partition_end;
+	int system_bak_partition_start;
+	int system_bak_partition_end;
 	int vendor_partition_start;
 	int vendor_partition_end;
 	int userdata_partition_start;
+	int userdata_partition_end;
+	int userdata_bak_partition_start;
+	int userdata_bak_partition_end;
 	int partition_size;
 	char mkpart_command[70];
 	check_platfrom_and_parted_windows(); //如果需要在Linux/UNIX下运行此程序,请注释此行,并将下一行取消注释
@@ -351,6 +357,112 @@ int main(void)
 				system_plus("adb pull fsc.img /dev/block/mmcblk0p51");
 				printf("恢复完成\n");
 				break;
+			case 23: //开始安装双系统(64GB主板)
+				userdata_bak_partition_end = 62500;
+				init();
+				confirm_operation();
+				//用户输入自定义容量并计算分区起始位置
+				partition_size = enter_system_partition_size();
+				system_bak_partition_start = system_partition_end = system_partition_start + partition_size;
+				vendor_partition_start = system_bak_partition_end = system_bak_partition_start + partition_size;
+				partition_size = enter_vendor_partition_size();
+				userdata_partition_start = vendor_partition_end = vendor_partition_start + partition_size;
+				userdata_bak_partition_start = userdata_partition_end = (int)((userdata_bak_partition_end - userdata_partition_start) / 2) + userdata_partition_start;
+				umount_partition();
+				parted_rm_partition2();
+				printf("建立新的System分区...\n");
+				sprintf(mkpart_command, "adb shell parted /dev/block/mmcblk0 mkpart system EXT4 %d %d", system_partition_start, system_partition_end);
+				system_plus(mkpart_command); //建立System分区
+				printf("建立新的System_bak分区...\n");
+				sprintf(mkpart_command, "adb shell parted /dev/block/mmcblk0 mkpart system_bak EXT4 %d %d", system_bak_partition_start, system_bak_partition_end);
+				system_plus(mkpart_command); //建立System_bak分区
+				printf("建立新的Vendor分区...\n");
+				sprintf(mkpart_command, "adb shell parted /dev/block/mmcblk0 mkpart vendor EXT4 %d %d", vendor_partition_start, vendor_partition_end);
+				system_plus(mkpart_command); //建立Vendor分区
+				printf("建立新的Userdata分区...\n");
+				sprintf(mkpart_command, "adb shell parted /dev/block/mmcblk0 mkpart userdata EXT4 %d %d", userdata_partition_start, userdata_partition_end);
+				system_plus(mkpart_command); //建立Userdata分区
+				printf("建立新的Userdata_bak分区...\n");
+				sprintf(mkpart_command, "adb shell parted /dev/block/mmcblk0 mkpart userdata_bak EXT4 %d %d", userdata_bak_partition_start, userdata_bak_partition_end);
+				system_plus(mkpart_command); //建立Userdata_bak分区
+				printf("双系统分区成功\n");
+				break;
+			case 24: //开始安装双系统(128GB主板)
+				userdata_bak_partition_end = 125000;
+				init();
+				confirm_operation();
+				//用户输入自定义容量并计算分区起始位置
+				partition_size = enter_system_partition_size();
+				system_bak_partition_start = system_partition_end = system_partition_start + partition_size;
+				vendor_partition_start = system_bak_partition_end = system_bak_partition_start + partition_size;
+				partition_size = enter_vendor_partition_size();
+				userdata_partition_start = vendor_partition_end = vendor_partition_start + partition_size;
+				userdata_bak_partition_start = userdata_partition_end = (int)((userdata_bak_partition_end - userdata_partition_start) / 2) + userdata_partition_start;
+				umount_partition();
+				parted_rm_partition2();
+				printf("建立新的System分区...\n");
+				sprintf(mkpart_command, "adb shell parted /dev/block/mmcblk0 mkpart system EXT4 %d %d", system_partition_start, system_partition_end);
+				system_plus(mkpart_command); //建立System分区
+				printf("建立新的System_bak分区...\n");
+				sprintf(mkpart_command, "adb shell parted /dev/block/mmcblk0 mkpart system_bak EXT4 %d %d", system_bak_partition_start, system_bak_partition_end);
+				system_plus(mkpart_command); //建立System_bak分区
+				printf("建立新的Vendor分区...\n");
+				sprintf(mkpart_command, "adb shell parted /dev/block/mmcblk0 mkpart vendor EXT4 %d %d", vendor_partition_start, vendor_partition_end);
+				system_plus(mkpart_command); //建立Vendor分区
+				printf("建立新的Userdata分区...\n");
+				sprintf(mkpart_command, "adb shell parted /dev/block/mmcblk0 mkpart userdata EXT4 %d %d", userdata_partition_start, userdata_partition_end);
+				system_plus(mkpart_command); //建立Userdata分区
+				printf("建立新的Userdata_bak分区...\n");
+				sprintf(mkpart_command, "adb shell parted /dev/block/mmcblk0 mkpart userdata_bak EXT4 %d %d", userdata_bak_partition_start, userdata_bak_partition_end);
+				system_plus(mkpart_command); //建立Userdata_bak分区
+				printf("双系统分区成功\n");
+				break;
+			case 25: //切换至系统一
+				init();
+				confirm_operation();
+				system_plus("adb shell parted /dev/block/mmcblk0 name 66 system");
+				system_plus("adb shell parted /dev/block/mmcblk0 name 67 system_bak");
+				system_plus("adb shell parted /dev/block/mmcblk0 name 70 userdata");
+				system_plus("adb shell parted /dev/block/mmcblk0 name 71 userdata_bak");
+				system_plus("adb reboot");
+				printf("切换至系统一成功\n");
+				break;
+			case 26: //切换至系统二
+				init();
+				confirm_operation();
+				system_plus("adb shell parted /dev/block/mmcblk0 name 67 system");
+				system_plus("adb shell parted /dev/block/mmcblk0 name 66 system_bak");
+				system_plus("adb shell parted /dev/block/mmcblk0 name 71 userdata");
+				system_plus("adb shell parted /dev/block/mmcblk0 name 70 userdata_bak");
+				system_plus("adb reboot");
+				printf("切换至系统二成功\n");
+				break;
+			case 27: //还原分区表(64GB主板)
+				init();
+				confirm_operation();
+				umount_partition();
+				parted_rm_partition3();
+				printf("恢复System分区...\n");
+                system_plus("adb shell parted /dev/block/mmcblk0 mkpart system EXT4 1040 4521");
+                printf("恢复Vendor分区...\n");
+                system_plus("adb shell parted /dev/block/mmcblk0 mkpart vendor EXT4 4521 5595");
+                printf("恢复Userdata分区...\n");
+                system_plus("adb shell parted /dev/block/mmcblk0 mkpart userdata EXT4 5604 62.5g");
+				printf("恢复完成\n");
+				break;
+			case 28: //还原分区表(128GB主板)
+				init();
+				confirm_operation();
+				umount_partition();
+				parted_rm_partition3();
+				printf("恢复System分区...\n");
+                system_plus("adb shell parted /dev/block/mmcblk0 mkpart system EXT4 1040 4521");
+                printf("恢复Vendor分区...\n");
+                system_plus("adb shell parted /dev/block/mmcblk0 mkpart vendor EXT4 4521 5595");
+                printf("恢复Userdata分区...\n");
+                system_plus("adb shell parted /dev/block/mmcblk0 mkpart userdata EXT4 5604 125g");
+				printf("恢复完成\n");
+				break;
 		}
 		bar1();
 		printf("按回车键返回主菜单");
@@ -461,6 +573,20 @@ void parted_rm_partition2(void)
     system_plus("adb shell parted /dev/block/mmcblk0 rm 69");
 	bar2();
 }
+void parted_rm_partition3(void)
+{
+	printf("删除System分区...\n");
+    system_plus("adb shell parted /dev/block/mmcblk0 rm 66");
+    printf("删除System_bak分区...\n");
+    system_plus("adb shell parted /dev/block/mmcblk0 rm 67");
+    printf("删除Vendor分区...\n");
+    system_plus("adb shell parted /dev/block/mmcblk0 rm 69");
+	printf("删除Userdata分区...\n");
+    system_plus("adb shell parted /dev/block/mmcblk0 rm 70");
+	printf("删除Userdata_bak分区...\n");
+    system_plus("adb shell parted /dev/block/mmcblk0 rm 71");
+	bar2();
+}
 void clearscreen(void)
 {
 	system("CLS");
@@ -515,6 +641,7 @@ int menu(void)
 	printf("- 03.清除功能区\n");
 	printf("- 04.128GB主板专区\n");
 	printf("- 05.实用功能区\n");
+	printf("- 06.双系统功能区\n");
 	printf("\n");
 	bar2();
 	printf("- 0.退出                           版本:%s\n", VERSION);
@@ -539,6 +666,7 @@ int menu(void)
 					printf("\n");
 					printf("- 01.Fastboot重启到Recovery\n");
 					printf("- 02.开机状态重启到Recovery\n");
+					printf("\n");
 					printf("\n");
 					printf("\n");
 					printf("\n");
@@ -585,6 +713,7 @@ int menu(void)
 					printf("\n");
 					printf("\n");
 					printf("\n");
+					printf("\n");
 					bar2();
 					printf("- 0.返回主菜单\n");
 					bar1();
@@ -625,6 +754,7 @@ int menu(void)
 					printf("- 12.通用格式化Data分区(需要TWRP支持)\n");
 					printf("- 13.格式化Data分区_R11\n");
 					printf("- 14.格式化Data分区_R11s\n");
+					printf("\n");
 					printf("\n");
 					printf("\n");
 					bar2();
@@ -671,6 +801,7 @@ int menu(void)
 					printf("\n");
 					printf("\n");
 					printf("\n");
+					printf("\n");
 					bar2();
 					printf("- 0.返回主菜单\n");
 					bar1();
@@ -712,6 +843,7 @@ int menu(void)
 					printf("\n");
 					printf("\n");
 					printf("\n");
+					printf("\n");
 					bar2();
 					printf("- 0.返回主菜单\n");
 					bar1();
@@ -728,6 +860,50 @@ int menu(void)
 							case 20:
 							case 21:
 							case 22:
+								clearscreen();
+								return choose2; //主函数对应功能
+							default:
+								clearscreen();
+								return -1; //返回值-1，由主函数去除错误输入
+						}
+					}
+					else
+						clearscreen();
+						return -1; //返回值-1，由主函数去除错误输入
+				case 6:
+					bar1();
+					printf(" <                                               \n");
+					bar2();
+					printf("               ==双系统功能区==                \n");
+					printf("用前声明:\n");
+					printf(" 1.在使用此处功能前请确保您具备救砖的能力\n");
+					printf(" 2.双系统将共用Boot,Vendor和Firmware,用户数据平分\n");
+					printf(" 3.两个系统只能为其中一个系统设置密码\n");
+					bar2();
+					printf("- 23.开始安装双系统(64GB主板)\n");
+					printf("- 24.开始安装双系统(128GB主板)\n");
+					printf("- 25.切换至系统一\n");
+					printf("- 26.切换至系统二\n");
+					printf("- 27.还原分区表(64GB主板)\n");
+					printf("- 28.还原分区表(128GB主板)\n");
+					bar2();
+					printf("- 0.返回主菜单\n");
+					bar1();
+					printf("请输入你想要使用的功能序号:");
+					if (scanf("%d", &choose2) == 1)
+					{
+						safe_flush(stdin); //清除stdin
+						switch(choose2)
+						{
+							case 0:
+								clearscreen();
+								return -3; //由主函数返回主菜单
+							case 23:
+							case 24:
+							case 25:
+							case 26:
+							case 27:
+							case 28:
 								clearscreen();
 								return choose2; //主函数对应功能
 							default:
@@ -756,11 +932,11 @@ int enter_system_partition_size(void)
 	int partition_size;
 	while (1)
     {
-        printf("请输入您要扩容的System分区大小(单位为MB,范围为3320~10240):"); //限定范围，防止误操作
+        printf("请输入您要扩容的System分区大小(单位为MB,范围为3481~10240):"); //限定范围，防止误操作
         if (scanf("%d", &partition_size) == 1)
 		{
         	safe_flush(stdin); //清除stdin
-        	if (partition_size <= 10240 && partition_size >= 3320)
+        	if (partition_size <= 10240 && partition_size >= 3481)
             	return partition_size;
         	else
         	{
@@ -781,11 +957,11 @@ int enter_vendor_partition_size(void)
 	int partition_size;
 	while (1)
     {
-        printf("请输入您要扩容的Vendor分区大小(单位为MB,范围为1024~3072):"); //限定范围，防止误操作
+        printf("请输入您要扩容的Vendor分区大小(单位为MB,范围为1074~3072):"); //限定范围，防止误操作
         if (scanf("%d", &partition_size) == 1)
 		{
         	safe_flush(stdin); //清除stdin
-        	if (partition_size <= 3072 && partition_size >= 1024)
+        	if (partition_size <= 3072 && partition_size >= 1074)
             	return partition_size;
         	else
         	{
